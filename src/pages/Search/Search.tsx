b01e518e5eea4e436fe3a2e8d74000c6afa76da9
yunji-cho <pplusmessage@gmail.com>
2022-07-17 00:15:06 +0900

    feat: search list 작성

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useState } from 'react'
+import { FormEvent, useEffect, useState } from 'react'
 import { useQuery } from 'react-query'
 import { useRecoil } from 'hooks/state'
 import { getMusicSheetApi } from 'service/getMusicSheetApi'
@@ -23,14 +23,15 @@ import Tag from './Tag/Tag'
 
 const Search = () => {
   const [visibleItem, setVisibleItem] = useState(false)
-  const [filtered, setFiltered] = useState<IResultData[] | undefined>([])
+  const [filterTitle, setFilterTitle] = useState<IResultData[] | undefined>([])
+  const [filterContent, setFilterContent] = useState<IResultData[] | undefined>([])
+  const [totalLength, setTotalLength] = useState(0)
   const [alertMessage, setAlertMessage] = useState('')
   const [searchText] = useRecoil(searchTextState)
   const [textFilter] = useRecoil(searchTextFilterState)
   const [code] = useRecoil(searchMusicCodeState)
   const [category] = useRecoil(searchCategoryState)
   const [, setFilterModal] = useRecoil(filterModalState)
-
   const [confirmModal, setConfirmModal] = useRecoil(confirmModalState)
 
   const { data } = useQuery(['musicSheets'], () => getMusicSheetApi().then((res) => res.data), {
@@ -39,9 +40,26 @@ const Search = () => {
   })
 
   const filterData = () => {
-    const getData = data?.results
-    const filteredData = getData?.filter((item) => item.title.includes(searchText))
-    setFiltered(filteredData)
+    if (!data) return
+    const getData = data.results
+    let filteredTitle
+    let filteredContent
+
+    if (textFilter === 'Any') {
+      filteredTitle = getData.filter((item) => item.title.includes(searchText))
+      filteredContent = getData.filter((item) => item.article.includes(searchText))
+    }
+    if (textFilter === 'Title') {
+      filteredTitle = getData.filter((item) => item.title.includes(searchText))
+      filteredContent = []
+    }
+    if (textFilter === 'Content') {
+      filteredTitle = []
+      filteredContent = getData.filter((item) => item.article.includes(searchText))
+    }
+
+    setFilterTitle(filteredTitle)
+    setFilterContent(filteredContent)
   }
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
@@ -58,6 +76,16 @@ const Search = () => {
   const handleFilterModal = () => {
     setFilterModal((prev) => !prev)
   }
+
+  useEffect(() => {
+    let titleDataLength = 0
+    let contentDataLength = 0
+
+    if (filterTitle) titleDataLength = filterTitle.length
+    if (filterContent) contentDataLength = filterContent.length
+
+    setTotalLength(titleDataLength + contentDataLength)
+  }, [filterTitle, filterContent])
 
   const tagArr = [
     { title: 'textFilter', value: textFilter },
@@ -84,14 +112,38 @@ const Search = () => {
       </form>
       {visibleItem && (
         <div className={styles.result}>
-          <p className={styles.length}>검색결과 총 {filtered?.length}건을 찾았습니다.</p>
-          {/* {filtered && (
-              {filtered.map((item) => (
-                <Item key={item.id} {...item} />
-              ))}
-            <ul>
-            </ul>
-          )} */}
+          <p className={styles.length}>검색결과 총 {totalLength}건을 찾았습니다.</p>
+
+          {filterTitle && filterTitle.length > 0 && (
+            <div className={styles.titleArea}>
+              <p className={styles.title}>Title ({filterTitle.length})</p>
+              <ul className={styles.list}>
+                {filterTitle.map((item) => {
+                  return (
+                    <li className={styles.item} key={item.id}>
+                      <p>{item.title}</p>
+                      <span>{item.musicCode}</span>
+                    </li>
+                  )
+                })}
+              </ul>
+            </div>
+          )}
+          {filterContent && filterContent.length > 0 && (
+            <div className={styles.titleArea}>
+              <p className={styles.title}>Content ({filterContent.length})</p>
+              <ul className={styles.list}>
+                {filterContent.map((item) => {
+                  return (
+                    <li className={styles.item} key={item.id}>
+                      <p>{item.title}</p>
+                      <span>{item.musicCode}</span>
+                    </li>
+                  )
+                })}
+              </ul>
+            </div>
+          )}
         </div>
       )}
 
